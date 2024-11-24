4e7dc1c679daddca93f9afb435d44c48525d073b
Zachary Malinka <zacharymalinka@gmail.com>
2024-11-23 21:08:17 -0500

    first commit

---
+++ src/components/Blue.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Blue() {
+  return (
+    <div className="Blue">
+      <h1>Blue</h1>
+    </div>
+  );
+}
---
+++ src/components/Red.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Red() {
+  return (
+    <div className="Red">
+      <h1>Red</h1>
+    </div>
+  );
+}
